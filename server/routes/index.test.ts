57284ce5c4edaca83d2196756203556b40a3f892
James King <james.king@tpximpact.com>
2026-06-25 15:53:26 +0100

    Reintroduce basic test

---
+++ server/routes/index.test.ts
@@ -0,0 +1,36 @@
+import type { Express } from 'express'
+import request from 'supertest'
+import { appWithAllRoutes, user } from './testutils/appSetup'
+import AuditService from '../services/auditService'
+import HmppsAuditClient from '../data/hmppsAuditClient'
+
+jest.mock('../services/auditService')
+
+const auditService = new AuditService({} as HmppsAuditClient) as jest.Mocked<AuditService>
+
+let app: Express
+
+beforeEach(() => {
+  app = appWithAllRoutes({
+    services: {
+      auditService,
+    },
+    userSupplier: () => user,
+  })
+})
+
+afterEach(() => {
+  jest.resetAllMocks()
+})
+
+describe('GET /', () => {
+  it('should render the index page', () => {
+    return request(app)
+      .get('/')
+      .expect('Content-Type', /html/)
+      .expect(200)
+      .expect(res => {
+        expect(res.text).toContain('This site is under construction...')
+      })
+  })
+})
