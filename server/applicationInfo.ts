e5119aa008da87e52a4d93722fe96dfffc09a1b4
Maddie Williams <maddie.williams1@gmail.com>
2026-07-01 13:07:06 +0100

    Fix build error

--- server/applicationInfo.ts
+++ server/applicationInfo.ts
@@ -15,7 +15,16 @@ export type ApplicationInfo = {
 }
 
 export default (): ApplicationInfo => {
-  const packageJson = path.join(__dirname, '../package.json')
+  const packageJson = path.join(process.cwd(), 'package.json')
   const { name: applicationName, version } = JSON.parse(fs.readFileSync(packageJson).toString())
-  return { applicationName, version, buildNumber, gitRef, gitShortHash: gitRef.substring(0, 7), productId, branchName }
+
+  return {
+    applicationName,
+    version,
+    buildNumber,
+    gitRef,
+    gitShortHash: gitRef.substring(0, 7),
+    productId,
+    branchName,
+  }
 }
