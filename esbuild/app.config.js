92cb80eab40e9b1991b4a0610aeca0b086ae9b14
James King <james.king@tpximpact.com>
2026-07-09 10:47:47 +0100

    Update ESBUILD step to be inline with the template

--- esbuild/app.config.js
+++ esbuild/app.config.js
@@ -1,33 +1,26 @@
-/* eslint-disable no-console */
 const { copy } = require('esbuild-plugin-copy')
 const { typecheckPlugin } = require('@jgoz/esbuild-plugin-typecheck')
-const esbuild = require('esbuild')
-const glob = require('glob')
-const pkg = require('../package.json')
+const { globSync } = require('node:fs')
+const { buildNotificationPlugin } = require('./utils')
 
-const buildApp = buildConfig =>
-  esbuild.build({
-    entryPoints: glob.sync(buildConfig.app.entryPoints),
-    outdir: buildConfig.app.outDir,
-    bundle: buildConfig.isProduction,
-    sourcemap: !buildConfig.isProduction,
-    platform: 'node',
-    format: 'cjs',
-    external: buildConfig.isProduction
-      ? [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})]
-      : [],
-    plugins: [
-      typecheckPlugin(),
-      copy({
-        resolveFrom: 'cwd',
-        assets: buildConfig.app.copy,
-      }),
-    ],
-  })
+/**
+ * Build typescript application into CommonJS
+ */
+const getAppConfig = buildConfig => ({
+  entryPoints: globSync(buildConfig.app.entryPoints),
+  outdir: buildConfig.app.outDir,
+  bundle: false,
+  sourcemap: true,
+  platform: 'node',
+  format: 'cjs',
+  plugins: [
+    typecheckPlugin({ watch: buildConfig.isWatchMode }),
+    copy({
+      resolveFrom: 'cwd',
+      assets: buildConfig.app.copy,
+    }),
+    buildNotificationPlugin('App', buildConfig.isWatchMode),
+  ],
+})
 
-module.exports = buildConfig => {
-  console.log('\u{1b}[1m\u{2728}  Building app...\u{1b}[0m')
-  console.log(`Building with isProduction set to ${buildConfig.isProduction} `)
-
-  return buildApp(buildConfig)
-}
+module.exports = { getAppConfig }
--- esbuild/assets.config.js
+++ esbuild/assets.config.js
@@ -1,56 +1,49 @@
-/* eslint-disable no-console */
-
-const autoprefixer = require('autoprefixer')
-const postcss = require('postcss')
-const postcssPresetEnv = require('postcss-preset-env')
+const path = require('node:path')
 const { copy } = require('esbuild-plugin-copy')
 const { sassPlugin } = require('esbuild-sass-plugin')
-const { clean } = require('esbuild-plugin-clean')
-const esbuild = require('esbuild')
-const path = require('path')
-const { glob } = require('glob')
+const manifestPlugin = require('esbuild-plugin-manifest')
+const { globSync } = require('node:fs')
+const { buildNotificationPlugin, cleanPlugin } = require('./utils')
 
-const buildAdditionalAssets = buildConfig =>
-  esbuild.build({
-    outdir: buildConfig.assets.outDir,
-    plugins: [
-      copy({
-        resolveFrom: 'cwd',
-        assets: buildConfig.assets.copy,
-      }),
-    ],
-  })
+/**
+ * Copy additional assets into distribution
+ */
+const getAdditionalAssetsConfig = buildConfig => ({
+  outdir: buildConfig.assets.outDir,
+  plugins: [
+    copy({
+      resolveFrom: 'cwd',
+      assets: buildConfig.assets.copy,
+    }),
+    buildNotificationPlugin('Assets (Additional)', buildConfig.isWatchMode),
+  ],
+})
 
-const buildAssets = buildConfig =>
-  esbuild.build({
-    entryPoints: buildConfig.assets.entryPoints,
-    outdir: buildConfig.assets.outDir,
-    entryNames: '[ext]/app',
-    minify: buildConfig.isProduction,
-    sourcemap: buildConfig.sourcemap,
-    platform: 'browser',
-    target: 'es2023',
-    external: ['/assets/*'],
-    bundle: true,
-    plugins: [
-      clean({
-        patterns: glob.sync(buildConfig.assets.clear),
-      }),
-      sassPlugin({
-        quietDeps: true,
-        loadPaths: [process.cwd(), path.join(process.cwd(), 'node_modules')],
-        async transform(source, resolveDir) {
-          const { css } = await postcss([autoprefixer, postcssPresetEnv({ stage: 0 })]).process(source, {
-            from: undefined,
-          })
-          return css
-        },
-      }),
-    ],
-  })
+/**
+ * Build scss and javascript assets
+ */
+const getAssetsConfig = buildConfig => ({
+  entryPoints: buildConfig.assets.entryPoints,
+  outdir: buildConfig.assets.outDir,
+  entryNames: '[ext]/[name].[hash]',
+  minify: buildConfig.isProduction,
+  sourcemap: !buildConfig.isProduction,
+  platform: 'browser',
+  target: 'es2018',
+  external: ['/assets/*'],
+  bundle: true,
+  plugins: [
+    cleanPlugin(globSync(buildConfig.assets.clear)),
+    manifestPlugin({
+      generate: entries =>
+        Object.fromEntries(Object.entries(entries).map(paths => paths.map(p => p.replace(/^dist\//, '/')))),
+    }),
+    sassPlugin({
+      quietDeps: true,
+      loadPaths: [process.cwd(), path.join(process.cwd(), 'node_modules')],
+    }),
+    buildNotificationPlugin('Assets', buildConfig.isWatchMode),
+  ],
+})
 
-module.exports = buildConfig => {
-  console.log('\u{1b}[1m\u{2728}  Building assets...\u{1b}[0m')
-
-  return Promise.all([buildAssets(buildConfig), buildAdditionalAssets(buildConfig)])
-}
+module.exports = { getAssetsConfig, getAdditionalAssetsConfig }
