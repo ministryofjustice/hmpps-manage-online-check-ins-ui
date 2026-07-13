2421edf20932d8cca1075079049f6aadaa9f7222
James King <james.king@tpximpact.com>
2026-07-13 10:06:34 +0100

    Attempted fix for integration tests on the pipelines

--- wiremock/stubs/index.ts
+++ wiremock/stubs/index.ts
@@ -1,8 +1,28 @@
-import superagent, { Response } from 'superagent'
+import fs from 'fs'
+import path from 'path'
+import superagent from 'superagent'
 import eSupervisionAPI from './eSupervisionAPI'
 
+const wiremockAdminUrl = 'http://localhost:9091/__admin'
+const mappingsDir = path.resolve(__dirname, '../mappings')
+
+const importFileMappings = (): Promise<superagent.Response[]> =>
+  Promise.all(
+    fs
+      .readdirSync(mappingsDir)
+      .filter(file => file.endsWith('.json'))
+      .map(file =>
+        superagent
+          .post(`${wiremockAdminUrl}/mappings/import`)
+          .send(JSON.parse(fs.readFileSync(path.join(mappingsDir, file), 'utf8'))),
+      ),
+  )
+
 export default {
-  resetMocks: (): Promise<Array<Response>> =>
-    Promise.all([superagent.post('http://localhost:9091/__admin/mappings/reset')]),
+  resetMocks: async (): Promise<null> => {
+    await superagent.post(`${wiremockAdminUrl}/mappings/reset`)
+    await importFileMappings()
+    return null
+  },
   ...eSupervisionAPI,
 }
